d51e03e84d8ccdd21b2245577a66f2aaf1d45ef2
manish.gaur@revinfotech.com <manish4revinfotech.com>
2020-07-29 15:21:05 +0530

    Frontend API

---
+++ Frontend/build/precache-manifest.18c3250248244a43ed5e305e61223153.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510eff6f7dcb1abc5ed19280ace715a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67c133031bfa68a20776",
+    "url": "/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "ee9601af73b7e5440d97",
+    "url": "/static/css/main.b664f414.chunk.css"
+  },
+  {
+    "revision": "67c133031bfa68a20776",
+    "url": "/static/js/2.caf54000.chunk.js"
+  },
+  {
+    "revision": "f719f401d106a3941d6dbff5d850f962",
+    "url": "/static/js/2.caf54000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9601af73b7e5440d97",
+    "url": "/static/js/main.070bf2a7.chunk.js"
+  },
+  {
+    "revision": "1a8ef731c821d2edac05",
+    "url": "/static/js/runtime-main.337ac1dc.js"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/static/media/SegoeUI.9a293118.woff"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/static/media/SegoeUIBold.52382539.woff"
+  }
+]);
